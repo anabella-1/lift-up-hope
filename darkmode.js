941e19bd7e9df4235add36643b9648837672b212
kalyesubulawilfredkevin <kwilfred151@gmail.com>
2026-02-17 13:30:27 +0300

    liftuphope

--- darkmode.js
+++ darkmode.js
@@ -4,3 +4,9 @@ document.getElementById("darkToggle");
 toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
+ document.getElementById("menuToggle");
+const nav = document.querySelector("navmenu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
